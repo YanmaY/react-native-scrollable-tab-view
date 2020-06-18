5b1477f73e59b7fbc27044a64317f70fb8470ab0
Wang Zhe <wx51yz@qq.com>
2020-06-18 16:05:32 +0800

    DefaultTabBar添加tab字体大小配置

--- DefaultTabBar.js
+++ DefaultTabBar.js
@@ -1,13 +1,8 @@
 const React = require('react');
-const { ViewPropTypes } = ReactNative = require('react-native');
+const { ViewPropTypes } = (ReactNative = require('react-native'));
 const PropTypes = require('prop-types');
 const createReactClass = require('create-react-class');
-const {
-  StyleSheet,
-  Text,
-  View,
-  Animated,
-} = ReactNative;
+const { StyleSheet, Text, View, Animated } = ReactNative;
 const Button = require('./Button');
 
 const DefaultTabBar = createReactClass({
@@ -17,43 +12,45 @@ const DefaultTabBar = createReactClass({
     tabs: PropTypes.array,
     backgroundColor: PropTypes.string,
     activeTextColor: PropTypes.string,
+    activeTextSize: PropTypes.string,
     inactiveTextColor: PropTypes.string,
+    inactiveTextSize: PropTypes.string,
     textStyle: Text.propTypes.style,
     tabStyle: ViewPropTypes.style,
     renderTab: PropTypes.func,
-    underlineStyle: ViewPropTypes.style,
+    underlineStyle: ViewPropTypes.style
   },
 
   getDefaultProps() {
     return {
       activeTextColor: 'navy',
       inactiveTextColor: 'black',
-      backgroundColor: null,
+      backgroundColor: null
     };
   },
 
-  renderTabOption(name, page) {
-  },
+  renderTabOption(name, page) {},
 
   renderTab(name, page, isTabActive, onPressHandler) {
-    const { activeTextColor, inactiveTextColor, textStyle, } = this.props;
+    const { activeTextColor, inactiveTextColor, activeTextSize, inactiveTextSize, textStyle } = this.props;
     const textColor = isTabActive ? activeTextColor : inactiveTextColor;
+    const textSize = isTabActive ? activeTextSize : inactiveTextSize;
     const fontWeight = isTabActive ? 'bold' : 'normal';
 
-    return <Button
-      style={{flex: 1, }}
-      key={name}
-      accessible={true}
-      accessibilityLabel={name}
-      accessibilityTraits='button'
-      onPress={() => onPressHandler(page)}
-    >
-      <View style={[styles.tab, this.props.tabStyle, ]}>
-        <Text style={[{color: textColor, fontWeight, }, textStyle, ]}>
-          {name}
-        </Text>
-      </View>
-    </Button>;
+    return (
+      <Button
+        style={{ flex: 1 }}
+        key={name}
+        accessible={true}
+        accessibilityLabel={name}
+        accessibilityTraits="button"
+        onPress={() => onPressHandler(page)}
+      >
+        <View style={[styles.tab, this.props.tabStyle]}>
+          <Text style={[{ color: textColor, fontSize: textSize, fontWeight }, textStyle]}>{name}</Text>
+        </View>
+      </Button>
+    );
   },
 
   render() {
@@ -64,15 +61,15 @@ const DefaultTabBar = createReactClass({
       width: containerWidth / numberOfTabs,
       height: 4,
       backgroundColor: 'navy',
-      bottom: 0,
+      bottom: 0
     };
 
     const translateX = this.props.scrollValue.interpolate({
       inputRange: [0, 1],
-      outputRange: [0,  containerWidth / numberOfTabs],
+      outputRange: [0, containerWidth / numberOfTabs]
     });
     return (
-      <View style={[styles.tabs, {backgroundColor: this.props.backgroundColor, }, this.props.style, ]}>
+      <View style={[styles.tabs, { backgroundColor: this.props.backgroundColor }, this.props.style]}>
         {this.props.tabs.map((name, page) => {
           const isTabActive = this.props.activeTab === page;
           const renderTab = this.props.renderTab || this.renderTab;
@@ -82,16 +79,14 @@ const DefaultTabBar = createReactClass({
           style={[
             tabUnderlineStyle,
             {
-              transform: [
-                { translateX },
-              ]
+              transform: [{ translateX }]
             },
-            this.props.underlineStyle,
+            this.props.underlineStyle
           ]}
         />
       </View>
     );
-  },
+  }
 });
 
 const styles = StyleSheet.create({
@@ -99,7 +94,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    paddingBottom: 10,
+    paddingBottom: 10
   },
   tabs: {
     height: 50,
@@ -109,8 +104,8 @@ const styles = StyleSheet.create({
     borderTopWidth: 0,
     borderLeftWidth: 0,
     borderRightWidth: 0,
-    borderColor: '#ccc',
-  },
+    borderColor: '#ccc'
+  }
 });
 
 module.exports = DefaultTabBar;
